c5449c4507f75e41f9c6cc0cabaebaaaf09492fe
HCMUSTooTired <22120364@student.hcmus.edu.vn>
2024-10-23 16:18:21 +0700

    Update App.xaml.cs

--- LearningManagementSystem/App.xaml.cs
+++ LearningManagementSystem/App.xaml.cs
@@ -45,7 +45,7 @@ namespace LearningManagementSystem
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
+            m_window = new DashBoard();
             m_window.Activate();
         }
 
