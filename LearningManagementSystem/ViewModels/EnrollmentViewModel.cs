0a911fb7df40c1db142cf0937ec1447c6ef71eae
Tuyen572004 <113967778+Tuyen572004@users.noreply.github.com>
2024-10-23 09:03:58 +0700

    add viewmodel

--- LearningManagementSystem/ViewModels/EnrollmentViewModel.cs
+++ LearningManagementSystem/ViewModels/EnrollmentViewModel.cs
@@ -4,50 +4,44 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LearningManagementSystem.DataAccess;
 using LearningManagementSystem.Models;
 
 namespace LearningManagementSystem.ViewModels
 {
-    public class EnrollmentClassesViewModel : BaseViewModel
-    {
-        private readonly object dao;
-
-        public ObservableCollection<EnrolledClassViewModel> EnrolledClasses { get; set; }
-
-        public EnrollmentClassesViewModel()
-        {
-            EnrolledClasses = new ObservableCollection<EnrolledClassViewModel>();
-            LoadEnrolledClasses();
-        }
-
-        private void LoadEnrolledClasses()
-        {
-            var studentId = 1; // Example student ID
-            var enrolledClasses = dao.GetEnrolledClassesByStudentId(studentId);
-
-            foreach (var enrolledClass in enrolledClasses)
-            {
-                var className = dao.GetClassNameByClassId(enrolledClass.ClassId);
-                var teacherId = dao.GetTeacherIdByClassId(enrolledClass.ClassId);
-                var courseId = dao.GetCourseIdByClassId(enrolledClass.ClassId);
-                var departmentId = dao.GetDepartmentIdByCourseId(courseId);
-                var departmentName = dao.GetDepartmentNameByDepartmentId(departmentId);
-                var teacherName = dao.GetTeacherNameByTeacherId(teacherId);
-
-                EnrolledClasses.Add(new EnrolledClassViewModel
-                {
-                    ClassName = className,
-                    TeacherName = teacherName,
-                    DepartmentName = departmentName
-                });
-            }
-        }
-    }
-
     public class EnrolledClassViewModel : BaseViewModel
     {
-        public string ClassName { get; set; }
-        public string TeacherName { get; set; }
-        public string DepartmentName { get; set; }
+        public Class Class { get; set; }
+        public ObservableCollection<Teacher> teachers { get; set; }
+        public Department Department { get; set; }
     }
+    public class EnrollmentClassesViewModel : BaseViewModel
+    {
+       public ObservableCollection<EnrolledClassViewModel> enrolledClasses { get; set; }
+        //public EnrollmentClassesViewModel()
+        //{
+        //    enrolledClasses = new ObservableCollection<EnrolledClassViewModel>();
+        //    LoadEnrolledClasses();
+        //}
+        //private void LoadEnrolledClasses()
+        //{
+        //    var studentId = 1; // Example student ID
+        //    var enrolledClasses = dao.GetEnrolledClassesByStudentId(studentId);
+
+        //    foreach (var enrolledClass in enrolledClasses)
+        //    {
+
+
+
+                  
+                
+
+        //        enrolledClasses.Add(new EnrolledClassViewModel
+        //        {
+        //        });
+        //    }
+        //}
+    }
+
+    
 }
